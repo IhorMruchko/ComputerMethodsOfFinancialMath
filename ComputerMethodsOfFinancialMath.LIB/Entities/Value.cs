bee064d051f250739ec07375d7e4c95bf60bcb3d
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2023-09-07 18:54:16 +0300

    Craete lib

---
+++ ComputerMethodsOfFinancialMath.LIB/Entities/Value.cs
@@ -0,0 +1,56 @@
+﻿using ComputerMethodsOfFinancialMath.LIB.Helpers;
+using System;
+
+namespace ComputerMethodsOfFinancialMath.LIB.Entities
+{
+    public class Value
+    {
+        public double MoneySum { get; protected set; }
+
+        public float InterestRate { get; protected set; }
+
+        public DateTime CurrentDate { get; protected set; } = DateTime.Now;
+
+
+        public Value ChangeRate(float interestRate)
+        {
+            if (interestRate < 0 || interestRate > 1)
+                throw new ArgumentException($"[{interestRate}] must be between 0 and 1.", nameof(interestRate));
+
+            InterestRate = interestRate;
+            return this;
+        }
+
+        public Value ChangeMoneySum(double moneySum)
+        {
+            if (moneySum <= 0)
+                throw new ArgumentException($"[{moneySum}] must be grater than zero.", nameof(moneySum));
+
+            return this;
+        }
+
+        public Value ChangeCurrentDate(DateTime date)
+        {
+            CurrentDate = date;
+            return this;
+        }
+
+        public static Value operator +(Value value, int years) 
+            => ValueHelpers.FutureDiscrete(value, years);
+
+        public static Value operator +(Value value, DateTime timePeriod)
+          => ValueHelpers.FutureContinuous(value, timePeriod);
+
+        public static Value operator -(Value value, int year) 
+            => ValueHelpers.PresentDiscrete(value, year);
+
+        public static Value operator -(Value value, DateTime timePerion)
+          => ValueHelpers.PresentContinuous(value, timePerion);
+
+
+        public static implicit operator Value(double money) 
+            => new Value()
+               .ChangeMoneySum(money)
+               .ChangeRate(0.1f);
+    }
+}
---
+++ ComputerMethodsOfFinancialMath.LIB/Helpers/TimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ComputerMethodsOfFinancialMath.LIB.Helpers
+{
+    public static class TimeHelper
+    {
+        public static float FromDateTime(DateTime first, DateTime second)
+           => FromDateTime(new DateTime(Math.Abs(first.Year - second.Year),
+                                        Math.Abs(first.Month - second.Month),
+                                        Math.Abs(first.Day - second.Day)));
+        public static float FromDateTime(DateTime deltaTime)
+            => deltaTime.Year + deltaTime.Month / 12f + deltaTime.Day / 365f;
+
+        public static bool IsFutureDate(DateTime present, DateTime future) 
+            => present.Year <= future.Year || present.Month <= future.Month || present.Day <= future.Day;
+
+        public static bool IsPastDate(DateTime present, DateTime past)
+            => present.Year >= past.Year || present.Month >= past.Month || present.Day >= past.Day;
+    }
+}
