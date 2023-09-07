b3c15cc687aef269ba94a22e8fa9a910a446876c
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2023-09-07 17:37:21 +0300

    Add project files.

---
+++ ComputerMethodsOfFinancialMath.WPF/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ComputerMethodsOfFinancialMath.WPF;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
